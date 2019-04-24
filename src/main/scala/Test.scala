d13b124a8a50e8dad98b5e115626fc7cf25c6f57
Huang HuiQi <838653850@qq.com>
2019-04-24 16:13:36 +0800

    增加可变函数和递归函数

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,5 +1,37 @@
 object testDemo{
   def main(args: Array[String]): Unit = {
+    Test()
     println("hello world!")
+    printString("Scala","Java","Python")//可变函数
+    for (i <- 1 to 10){
+      println(i + " 的阶乘为：= " + factorial(i))
+    }
   }
+
+  def Test(): Unit ={
+    val colors = Map("red" -> "#FF0000",
+      "azure" -> "#F0FFFF",
+      "peru" -> "#CD853F")
+
+    val nums: Map[Int, Int] = Map()
+
+    println( "colors 中的键为 : " + colors.keys )
+    println( "colors 中的值为 : " + colors.values )
+    println( "检测 colors 是否为空 : " + colors.isEmpty )
+    println( "检测 nums 是否为空 : " + nums.isEmpty )
+  }
+  def printString( args:String* ): Unit ={
+    var i :Int = 0;
+    for (arg <- args){
+      println("Arg value[" + i + "] = " +arg)
+      i = i +1;
+    }
+  }
+  def factorial(n: BigInt): BigInt={
+    if(n <= 1)
+      1
+    else
+      n * factorial(n - 1)
+  }
+
 }
